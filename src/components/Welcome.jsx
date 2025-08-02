5e9a4f402a3cf769fe048099e4096e2f7ea20218
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-08-02 15:00:18 +0530

    style: update the font color and styles

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,29 +3,30 @@ import { Button } from "@/components/ui/button";
 
 export default function Welcome() {
 
-    function Feature({ icon, title, desc }) {
-  return (
-    <div className="flex items-start gap-3">
-      <div className="pt-1">{icon}</div>
-      <div>
-        <h3 className="font-medium text-foreground">{title}</h3>
-        <p className="text-sm text-muted-foreground">{desc}</p>
+  function Feature({ icon, title, desc }) {
+    return (
+      <div className="flex items-start gap-3">
+        <div className="pt-1">{icon}</div>
+        <div>
+          <h3 className="font-medium text-foreground">{title}</h3>
+          <p className="text-sm text-muted-foreground">{desc}</p>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
   return (
-    <section className="max-w-3xl mx-auto py-10 px-6 text-center">
+    <section className="max-w-3xl mx-auto py-10 px-6 text-center mt-2 lg:mt-6">
       <h1 className="text-4xl font-bold text-foreground mb-6">
-        Welcome to <span className="text-primary">Quick.ai</span> 🚀
+        Welcome to <span className="text-gray-700 dark:text-gray-300">Promptcraft
+          <span className="text-primary">.ai</span></span> 🚀
       </h1>
 
       <p className="text-muted-foreground text-lg mb-6">
-        Quick.ai is your all-in-one AI toolkit designed to boost productivity by providing smart content tools, image editing features, and career enhancement services — all in one place.
+        Promptcraft.ai is your all-in-one AI toolkit designed to boost productivity by providing smart content tools, image editing features, and career enhancement services — all in one place.
       </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-left mb-10 px-8 py-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 text-left mb-10 px-8 py-2">
         <Feature icon={<Pen className="w-5 h-5 text-primary" />} title="AI Article Writer" desc="Create high-quality articles instantly." />
         <Feature icon={<Text className="w-5 h-5 text-purple-500" />} title="Blog Title Generator" desc="Generate catchy blog headlines." />
         <Feature icon={<ImagePlus className="w-5 h-5 text-green-500" />} title="AI Image Generation" desc="Generate visuals using AI." />
