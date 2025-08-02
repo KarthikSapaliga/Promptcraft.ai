5991b4df39fb5f094e87711d6c8a9bd15ea5169b
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-08-02 15:12:33 +0530

    style: change the icon size

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -6,15 +6,8 @@ export default function TopBar({ onSidebarToggle }) {
     const { theme, toggleTheme } = useTheme()
     return (
         <header className="w-full h-14 border-b bg-background flex items-center justify-between px-4">
-            <div className="flex items-center gap-2">
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    onClick={onSidebarToggle}
-                    className="text-gray-700 dark:text-gray-200 hover:bg-transparent"
-                >
-                    <Menu />
-                </Button>
+            <div className="flex items-center gap-4">
+                <Menu size={22}  onClick={onSidebarToggle} className="cursor-pointer rounded-sm hover:scale-105 hover:text-primary"/>
                 <div className="h-full flex gap-2 items-center">
                     <img
                         src="/logo.png"
@@ -34,7 +27,7 @@ export default function TopBar({ onSidebarToggle }) {
                 onClick={toggleTheme}
                 className="text-gray-700 dark:text-gray-200 hover:bg-transparent"
             >
-                {theme === "dark" ? <Sun size={22} /> : <Moon size={22} />}
+                {theme === "dark" ? <Sun size={24} /> : <Moon size={24} />}
             </Button>
         </header>
     );
