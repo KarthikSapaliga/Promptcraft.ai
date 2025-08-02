2cc84c2f241830612303b7a8bd73cd18bce8187f
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-08-02 12:49:35 +0530

    chore: add helper function to convert array buffer to base64

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function arrayBufferToBase64(buffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+
+  return btoa(binary);
+}
