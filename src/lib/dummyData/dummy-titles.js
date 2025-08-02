4cc0932e43768cec5b8a6693ac1f1075c2c314fb
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-08-02 06:52:49 +0530

    chore: add dummy titles data

---
+++ src/lib/dummyData/dummy-titles.js
@@ -0,0 +1,8 @@
+export const dummyTitles = [
+  "Unlocking the Future: The Rise of AI in Everyday Life",
+  "Why Artificial Intelligence is Everyone's Business Now",
+  "From Sci-Fi to Reality: AI's Role in Modern Society",
+  "Revolutionizing Business Strategy with AI Insights",
+  "Can Artificial Intelligence Drive Profitability?",
+  "The CEO's Guide to Adopting AI in Business"
+]
