b4d9535048143e557ddf7ca4924941fc688b7328
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-08-02 12:50:29 +0530

    feat: add controller for removing the image background

--- src/lib/ai-controller.js
+++ src/lib/ai-controller.js
@@ -9,6 +9,8 @@ import {
     getResumeReviewPrompt,
 } from "./prompts.js";
 
+import { arrayBufferToBase64 } from "./utils.js";
+
 const ai = new GoogleGenAI({
     apiKey: import.meta.env.VITE_GEMINI_API_KEY,
 });
@@ -67,14 +69,40 @@ export const generateImage = async (desc, style) => {
                 return `data:image/png;base64,${base64Image}`;
             }
         }
-
     } catch (err) {
         console.error("Error generating titles:", err);
         throw err;
     }
 };
 
-export const removeBg = async () => {};
+export const removeBg = async (imageBuffer) => {
+    const base64Image = arrayBufferToBase64(imageBuffer);
+    const prompt = getBgRemovalPrompt();
+
+    try {
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash-preview-image-generation",
+            contents: [
+                { text: prompt },
+                { inlineData: { mimeType: "image/png", data: base64Image } },
+            ],
+            config: {
+                responseModalities: [Modality.TEXT, Modality.IMAGE],
+            },
+        });
+
+        const parts = response.candidates[0]?.content?.parts || [];
+        for (const part of parts) {
+            if (part.inlineData?.data) {
+                const base64Image = part.inlineData.data;
+                return `data:image/png;base64,${base64Image}`;
+            }
+        }
+    } catch (err) {
+        console.error("Error generating titles:", err);
+        throw err;
+    }
+};
 
 export const removeObject = async () => {};
 
